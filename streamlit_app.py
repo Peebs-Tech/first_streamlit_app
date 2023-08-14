9e29d143700e607183cf557bd67481fcf389015b
Kin <91240031+Peebs-Tech@users.noreply.github.com>
2023-08-14 16:24:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-
+my_cur.execute("select * from fruit_load_list")
 my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
 
 
